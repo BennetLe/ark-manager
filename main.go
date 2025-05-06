edf3108a39be5b5231831a05f199bdefc07218ce
BennetLe <BennetLe@proton.me>
2025-05-06 12:30:06 +0200

    basic setup

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"ark-manager/view"
+	"ark-manager/view/layout"
+	"ark-manager/view/partial"
+	"log"
+	"net/http"
+
+	"github.com/a-h/templ"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	fs := http.FileServer(http.Dir("./static"))
+	http.Handle("/static/", http.StripPrefix("/static", fs))
+
+	c := layout.Base(view.Index())
+	http.Handle("/", templ.Handler(c))
+
+	http.Handle("/foo", templ.Handler(partial.Foo()))
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
