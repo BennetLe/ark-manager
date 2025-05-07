b6d07b421e3e4f4f74d81dc87b972673109a8684
BennetLe <BennetLe@proton.me>
2025-05-07 11:22:46 +0200

    added UserAuth middleware

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
 	"net/http"
 
 	"github.com/a-h/templ"
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-contrib/sessions/cookie"
 	"github.com/gin-gonic/gin"
 )
 
@@ -14,20 +16,46 @@ func main() {
 	router := gin.Default()
 	router.Static("/static/", "./static")
 
+	store := cookie.NewStore([]byte("secret")) // use a secure secret in production
+	router.Use(sessions.Sessions("session", store))
+
+	userAuth := router.Group("/")
+	userAuth.Use(UserAuth())
+
 	c := layout.Base(view.Index())
-	router.GET("/", gin.WrapH(templ.Handler(c)))
+	userAuth.GET("/", gin.WrapH(templ.Handler(c)))
 
 	router.GET("/login", gin.WrapH(templ.Handler(layout.Login())))
 	router.GET("/foo", gin.WrapH(templ.Handler(partial.Foo())))
 
-	router.POST("/login", func(ctx *gin.Context) {
-		username := ctx.PostForm("username")
-		password := ctx.PostForm("password")
-		ctx.JSON(http.StatusOK, gin.H{
-			"uname": username,
-			"pswd":  password,
-		})
-	})
+	router.POST("/login", loginHandler)
 
 	router.Run(":8080")
+}
+
+func UserAuth() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		session := sessions.Default(ctx)
+		user := session.Get("user")
+		if user == nil {
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"message": "Please log in first"})
+		}
+		ctx.Set("user", user)
+		ctx.Next()
+	}
+}
+
+func loginHandler(ctx *gin.Context) {
+	username := ctx.PostForm("username")
+	password := ctx.PostForm("password")
+
+	if password == "test" {
+		session := sessions.Default(ctx)
+		session.Set("user", username)
+		session.Save()
+		ctx.Redirect(http.StatusFound, "/")
+		return
+	}
+
+	ctx.JSON(http.StatusUnauthorized, gin.H{"message": "Unauthorized", "password": password})
 }
