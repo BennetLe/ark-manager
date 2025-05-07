c715a06afc1bdc1f069e62d5a8c5af411d316d80
BennetLe <BennetLe@proton.me>
2025-05-07 10:57:19 +0200

    basic login demo

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"ark-manager/view"
 	"ark-manager/view/layout"
 	"ark-manager/view/partial"
+	"net/http"
 
 	"github.com/a-h/templ"
 	"github.com/gin-gonic/gin"
@@ -16,7 +17,17 @@ func main() {
 	c := layout.Base(view.Index())
 	router.GET("/", gin.WrapH(templ.Handler(c)))
 
+	router.GET("/login", gin.WrapH(templ.Handler(layout.Login())))
 	router.GET("/foo", gin.WrapH(templ.Handler(partial.Foo())))
+
+	router.POST("/login", func(ctx *gin.Context) {
+		username := ctx.PostForm("username")
+		password := ctx.PostForm("password")
+		ctx.JSON(http.StatusOK, gin.H{
+			"uname": username,
+			"pswd":  password,
+		})
+	})
 
 	router.Run(":8080")
 }
